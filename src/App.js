720b2da8a5240f8707d11e7a73227b24d76c33ef
unknown <montasirazad@gmail.com>
2021-08-26 17:49:51 +0600

    4th

--- src/App.js
+++ src/App.js
@@ -2,34 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const people = ['Dhrubo','Fozu','Taki','Farabe','Uzzal','Tomal','kanon']
   return (
     <div className="App">
 
 
       <header className="App-header">
-        <Profile name='Dhrubo' age='27'></Profile>
-        <Profile name='Montasir' age='72'></Profile>
-        <Profile name='Azad' age='37'></Profile>
-         
+        
+        <Profile name='Dhrubo' age='27' address='taltola'></Profile>
+        <Profile name='Fozu' age='72' address='notunbag'></Profile>
+        <Profile name='Taki' age='37' address='c-block'></Profile>
+        <Profile name='Farabe' age='1000' address='bashabo'></Profile>
+        <Profile name='Uzzal' age='5' address='c-block'></Profile>
+        <Profile name='Tomal' age='57' address='b-block'></Profile>
+        <Profile name={people[6]} age='17' address='sipaibag'></Profile>  
+
       </header>
     </div>
   );
 
   function Profile(props) {
-  const profileStyle = {
-    width:"550px",
-    height:'200px',
-    border:'4px solid gold',
-    margin:'15px',
-    borderRadius:'8px'
-      
-  }
+    // console.log(props)
+    const profileStyle = {
+      width: "550px",
+      height: 'auto',
+      border: '4px solid gold',
+      margin: '15px',
+      borderRadius: '8px'
 
-    return(
-     <div style={profileStyle}>
-       <h1>Name : {props.name}</h1>
-       <h2>Age : {props.age}</h2>
-     </div>
+    }
+
+    return (
+      <div style={profileStyle}>
+        <h1>Name : {props.name}</h1>
+        <h2>Age : {props.age}</h2>
+        <h3>Address : {props.address}</h3>
+      </div>
     )
   }
 
